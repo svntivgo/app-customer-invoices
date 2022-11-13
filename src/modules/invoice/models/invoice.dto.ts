81087dc355c139dbaa4bd8c09ca6bd3f4dc93d53
Santiago Barrera <santiago.barrerae@gmail.com>
2022-11-13 17:59:23 -0500

    invoice dto

--- src/modules/invoice/models/invoice.dto.ts
+++ src/modules/invoice/models/invoice.dto.ts
@@ -1,1 +1,16 @@
-export class InvoiceDto {}
+import { IsString, IsNotEmpty, IsUUID, IsOptional } from "class-validator";
+import { DetailDto } from './detail.dto';
+
+export class InvoiceDto {
+  @IsString()
+  @IsNotEmpty()
+  @IsUUID()
+  id: string;
+
+  @IsString()
+  @IsNotEmpty()
+  date: string;
+
+  @IsOptional()
+  detail: DetailDto[];
+}
