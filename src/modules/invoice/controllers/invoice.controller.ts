e08c05fc8cc04dddc02f842e3a77b6575657d3f4
Santiago Barrera <santiago.barrerae@gmail.com>
2022-11-13 17:47:14 -0500

    dto validator ok

--- src/modules/invoice/controllers/invoice.controller.ts
+++ src/modules/invoice/controllers/invoice.controller.ts
@@ -1,9 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { brotliDecompressSync } from 'zlib';
+import { DetailDto } from '../models/detail.dto';
 
 @Controller('invoice')
 export class InvoiceController {
-  @Get()
-  helloWorld(): string {
-    return 'hello world';
+  @Post()
+  helloWorld(@Body() body: DetailDto): DetailDto {
+    return body;
   }
 }
--- src/modules/invoice/models/detail.dto.ts
+++ src/modules/invoice/models/detail.dto.ts
@@ -1,5 +1,13 @@
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
 export class DetailDto {
+  @IsString()
+  @IsNotEmpty()
   product: string;
+  @IsNumber()
+  @IsNotEmpty()
   amount: number;
+  @IsNumber()
+  @IsNotEmpty()
   unitPrice: number;
 }
