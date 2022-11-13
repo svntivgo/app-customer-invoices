73c66fb8873e34890ff796572c2eb5bd7c5efac8
Santiago Barrera <santiago.barrerae@gmail.com>
2022-11-13 18:26:33 -0500

    customer dto constructor

--- src/modules/customer/models/customer.dto.ts
+++ src/modules/customer/models/customer.dto.ts
@@ -1,4 +1,5 @@
 import { IsString, IsNotEmpty, IsEmail, IsOptional } from 'class-validator';
+import { randomUUID } from 'crypto';
 import { InvoiceDto } from '../../invoice/models/invoice.dto';
 export class CustomerDto {
   id: string;
@@ -14,5 +15,12 @@ export class CustomerDto {
   email: string;
 
   @IsOptional()
-  invoices: InvoiceDto[];
+  invoices?: InvoiceDto[];
+
+  constructor(name: string, email: string, invoices: InvoiceDto[]) {
+    this.id = randomUUID();
+    this.name = name;
+    this.email = email;
+    this.invoices = invoices;
+  }
 }
