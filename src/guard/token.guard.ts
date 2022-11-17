e262b86b874e88b22002aba6aa5c6921f1f08b22
Santiago Barrera <santiago.barrerae@gmail.com>
2022-11-16 23:07:22 -0500

    finalizacion reto

--- src/guard/token.guard.ts
+++ src/guard/token.guard.ts
@@ -12,6 +12,6 @@ export class AuthGuard implements CanActivate {
       .replace(' ', '');
     const token =
       '99e4c497d8b4c049ce41e71ff04055adc01714bccdf2f901e4c1d16ffabcef571398fa2b416ddc1037a98c4040c2f277c1bbb2dbc647ed65a2acaf657ac229ec';
-    return tokenReq === token ? true : false;
+    return tokenReq === token;
   }
 }
